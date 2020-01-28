cd37c23d02f16c691c6e573765b0ea3a1579db17
danilbakshaev <48173055+danilbakshaev@users.noreply.github.com>
2020-01-28 17:04:10 +0500

    Закончил меню

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,32 @@
-var headerTopWhite = document.querySelector(".page-header__top");
+var headerTop = document.querySelector(".page-header__top");
 var menuLinks = document.querySelector(".page-header__top__link");
-var menuBg = document.querySelector(".page-header__bg-menu");
+var menuBlur = document.querySelector(".page-header__blur");
 var menuOpenButton = document.querySelector(".page-header__top__menu");
+var headerWhite = document.querySelector(".page-header__white");
+var bodyDocument = document.querySelector("body");
 
 menuOpenButton.addEventListener("click", function(evt){
-    menuBg.classList.toggle("active");
-    menuLinks.classList.toggle("active");
-    headerTopWhite.classList.toggle("active");
+    if (!menuBlur.classList.contains("active")){
+        headerTop.classList.add("active");
+        headerWhite.classList.add("active");
+        menuBlur.classList.add("active");
+        menuLinks.classList.add("active");
+    } else {
+        menuBlur.classList.remove("active");
+        menuLinks.classList.remove("active");
+        if (pageYOffset === 0){
+            headerTop.classList.remove("active");
+            headerWhite.classList.remove("active");
+        }
+    }
+});
+
+window.addEventListener('scroll', function() {
+    if (pageYOffset === 0){
+        headerTop.classList.remove("active");
+        headerWhite.classList.remove("active");
+    } else {
+        headerTop.classList.add("active");
+        headerWhite.classList.add("active");
+    }
 });
